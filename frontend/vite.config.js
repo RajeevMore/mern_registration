f7372d23e83dbee1b2f5961e3ae9674d1ed3dd77
Rajeev More <100182072+RajeevMore@users.noreply.github.com>
2025-03-13 19:57:54 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-   server: {
-    allowedHosts: 'all'
+   plugins: [react()],
+  server: {
+    allowedHosts: ['www.mern.ibrainstar.in', 'mern.ibrainstar.in']
   }
 })
